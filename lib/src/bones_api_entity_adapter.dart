a729ca6a7cab9ff3124acb6efb21956255d24eae
gmpassos <gracilianomp@gmail.com>
2021-10-03 16:23:47 -0300

    reorganize methods order

--- lib/src/bones_api_entity_adapter.dart
+++ lib/src/bones_api_entity_adapter.dart
@@ -93,6 +93,79 @@ abstract class SQLAdapter<C> extends SchemeProvider
   @override
   void initialize();
 
+  /// If `true` indicates that this adapter SQL uses the `OUTPUT` syntax for inserts.
+  bool get sqlAcceptsInsertOutput;
+
+  /// If `true` indicates that this adapter SQL uses the `RETURNING` syntax for inserts.
+  bool get sqlAcceptsInsertReturning;
+
+  /// If `true` indicates that this adapter SQL uses the `IGNORE` syntax for inserts.
+  bool get sqlAcceptsInsertIgnore;
+
+  /// If `true` indicates that this adapter SQL uses the `ON CONFLICT` syntax for inserts.
+  bool get sqlAcceptsInsertOnConflict;
+
+  FutureOr<String> fieldValueToSQL(
+      EncodingContext context,
+      TableScheme tableScheme,
+      String fieldName,
+      Object value,
+      Map<String, Object?> fieldsValues) {
+    var fieldRef = tableScheme.fieldsReferencedTables[fieldName];
+
+    if (fieldRef == null) {
+      fieldsValues.putIfAbsent(fieldName, () => value);
+      var valueSQL = _conditionSQLGenerator.parameterPlaceholder(fieldName);
+      return valueSQL;
+    } else {
+      var refEntity = value;
+      var refEntityRepository = getEntityRepository(obj: refEntity);
+
+      if (refEntityRepository != null) {
+        var refId = refEntityRepository.entityHandler
+            .getField(value, fieldRef.targetField);
+
+        if (refId != null) {
+          fieldsValues.putIfAbsent(fieldName, () => refId);
+          var valueSQL = _conditionSQLGenerator.parameterPlaceholder(fieldName);
+          return valueSQL;
+        } else {
+          var retRefId = refEntityRepository.store(refEntity,
+              transaction: context.transaction);
+          return retRefId.resolveMapped((refId) {
+            fieldsValues.putIfAbsent(fieldName, () => refId);
+            var valueSQL =
+                _conditionSQLGenerator.parameterPlaceholder(fieldName);
+            return valueSQL;
+          });
+        }
+      } else {
+        if (value is Entity) {
+          var refId = value.getID();
+
+          fieldsValues.putIfAbsent(fieldName, () => refId);
+          var valueSQL = _conditionSQLGenerator.parameterPlaceholder(fieldName);
+          return valueSQL;
+        } else {
+          var reflection =
+              ReflectionFactory().getRegisterClassReflection(value.runtimeType);
+          if (reflection != null) {
+            var fieldId = tableScheme.idFieldName ?? 'id';
+
+            var refId = reflection.getField(fieldId, value);
+
+            fieldsValues.putIfAbsent(fieldName, () => refId);
+            var valueSQL =
+                _conditionSQLGenerator.parameterPlaceholder(fieldName);
+            return valueSQL;
+          }
+        }
+
+        return 'null';
+      }
+    }
+  }
+
   FutureOr<SQL> generateCountSQL(Transaction transaction, String table,
       {EntityMatcher? matcher,
       Object? parameters,
@@ -113,150 +186,18 @@ abstract class SQLAdapter<C> extends SchemeProvider
     }
   }
 
-  FutureOr<SQL> generateSelectSQL(
-      Transaction transaction, String table, EntityMatcher matcher,
-      {Object? parameters,
-      List? positionalParameters,
-      Map<String, Object?>? namedParameters,
-      int? limit}) {
-    return _generateSQLFrom(transaction, table, matcher,
-        parameters: parameters,
-        positionalParameters: positionalParameters,
-        namedParameters: namedParameters,
-        sqlBuilder: (String from, EncodingContext context) {
-      var tableAlias = context.resolveEntityAlias(table);
-      var limitStr = limit != null && limit > 0 ? ' LIMIT $limit' : '';
-      var sql = 'SELECT "$tableAlias".* $from$limitStr';
-      return sql;
+  FutureOr<int> countSQL(TransactionOperation op, String table, SQL sql) {
+    return executeTransactionOperation(op, (connection) {
+      _log.log(logging.Level.INFO, 'countSQL> $sql');
+      return doCountSQL(table, sql, connection);
     });
   }
 
-  FutureOr<SQL> generateDeleteSQL(
-      Transaction transaction, String table, EntityMatcher matcher,
-      {Object? parameters,
-      List? positionalParameters,
-      Map<String, Object?>? namedParameters}) {
-    return _generateSQLFrom(transaction, table, matcher,
-        parameters: parameters,
-        positionalParameters: positionalParameters,
-        namedParameters: namedParameters,
-        sqlBuilder: (String from, EncodingContext context) {
-      var tableAlias = context.resolveEntityAlias(table);
-
-      var sql = StringBuffer();
-      sql.write('DELETE ');
-
-      if (sqlAcceptsInsertOutput) {
-        sql.write(' OUTPUT DELETED.* ');
-      }
-
-      sql.write(from);
-
-      if (sqlAcceptsInsertReturning) {
-        sql.write(' RETURNING "$tableAlias".*');
-      }
-
-      return sql.toString();
-    });
-  }
-
-  FutureOr<SQL> _generateSQLFrom(
-      Transaction transaction, String table, EntityMatcher matcher,
-      {Object? parameters,
-      List? positionalParameters,
-      Map<String, Object?>? namedParameters,
-      required String Function(String from, EncodingContext context)
-          sqlBuilder}) {
-    if (matcher is Condition) {
-      var retEncodedSQL = _conditionSQLGenerator.encode(matcher, table,
-          parameters: parameters,
-          positionalParameters: positionalParameters,
-          namedParameters: namedParameters);
-
-      return retEncodedSQL.resolveMapped((encodedSQL) {
-        var conditionSQL = encodedSQL.outputString;
-
-        var tableAlias =
-            _conditionSQLGenerator.resolveEntityAlias(encodedSQL, table);
-
-        if (encodedSQL.fieldsReferencedTables.isEmpty) {
-          String from;
-          if (conditionSQL.isNotEmpty) {
-            from = 'FROM "$table" as "$tableAlias" WHERE $conditionSQL';
-          } else {
-            from = 'FROM $table as "$tableAlias"';
-          }
-
-          var sqlQuery = sqlBuilder(from, encodedSQL);
-
-          return SQL(sqlQuery, encodedSQL.parametersPlaceholders,
-              condition: matcher,
-              entityName: encodedSQL.entityName,
-              mainTable: table);
-        } else {
-          var referencedTablesFields = encodedSQL.referencedTablesFields;
-
-          var innerJoin = StringBuffer();
-
-          for (var e in referencedTablesFields.entries) {
-            var refTable = e.key;
-
-            var refTableAlias = encodedSQL.resolveEntityAlias(refTable);
-
-            innerJoin.write('INNER JOIN "$refTable" as "$refTableAlias" ON ');
-
-            for (var fieldRef in e.value) {
-              var sourceTableAlias = _conditionSQLGenerator.resolveEntityAlias(
-                  encodedSQL, fieldRef.sourceTable);
-              var targetTableAlias = _conditionSQLGenerator.resolveEntityAlias(
-                  encodedSQL, fieldRef.targetTable);
-
-              innerJoin.write('"');
-              innerJoin.write(sourceTableAlias);
-              innerJoin.write('"');
-              innerJoin.write('.');
-              innerJoin.write('"');
-              innerJoin.write(fieldRef.sourceField);
-              innerJoin.write('"');
-
-              innerJoin.write(' = ');
-
-              innerJoin.write('"');
-              innerJoin.write(targetTableAlias);
-              innerJoin.write('"');
-              innerJoin.write('.');
-              innerJoin.write('"');
-              innerJoin.write(fieldRef.targetField);
-              innerJoin.write('"');
-            }
-          }
-
-          var from =
-              'FROM "$table" as "$tableAlias" $innerJoin WHERE $conditionSQL';
-          var sqlQuery = sqlBuilder(from, encodedSQL);
-
-          return SQL(sqlQuery, encodedSQL.parametersPlaceholders,
-              condition: matcher,
-              entityName: encodedSQL.entityName,
-              mainTable: table);
-        }
-      });
-    } else {
-      throw StateError('$matcher');
-    }
-  }
-
-  /// If `true` indicates that this adapter SQL uses the `OUTPUT` syntax for inserts.
-  bool get sqlAcceptsInsertOutput;
-
-  /// If `true` indicates that this adapter SQL uses the `RETURNING` syntax for inserts.
-  bool get sqlAcceptsInsertReturning;
-
-  /// If `true` indicates that this adapter SQL uses the `IGNORE` syntax for inserts.
-  bool get sqlAcceptsInsertIgnore;
-
-  /// If `true` indicates that this adapter SQL uses the `ON CONFLICT` syntax for inserts.
-  bool get sqlAcceptsInsertOnConflict;
+  FutureOr<int> doCountSQL(
+    String table,
+    SQL sql,
+    C connection,
+  );
 
   FutureOr<SQL> generateInsertSQL(
       Transaction transaction, String table, Map<String, Object?> fields) {
@@ -313,6 +254,23 @@ abstract class SQLAdapter<C> extends SchemeProvider
       });
     });
   }
+
+  FutureOr<dynamic> insertSQL(TransactionOperation op, String table, SQL sql,
+      Map<String, Object?> fields,
+      {T Function<T>(dynamic o)? mapper}) {
+    return executeTransactionOperation(op, (connection) {
+      _log.log(logging.Level.INFO, 'insertSQL> $sql');
+      var retInsert = doInsertSQL(table, sql, connection);
+
+      if (mapper != null) {
+        return retInsert.resolveMapped((e) => mapper(e));
+      } else {
+        return retInsert;
+      }
+    });
+  }
+
+  FutureOr<dynamic> doInsertSQL(String table, SQL sql, C connection);
 
   FutureOr<SQL> generateUpdateSQL(Transaction transaction, String table,
       Object id, Map<String, Object?> fields) {
@@ -381,101 +339,12 @@ abstract class SQLAdapter<C> extends SchemeProvider
     });
   }
 
-  FutureOr<String> fieldValueToSQL(
-      EncodingContext context,
-      TableScheme tableScheme,
-      String fieldName,
-      Object value,
-      Map<String, Object?> fieldsValues) {
-    var fieldRef = tableScheme.fieldsReferencedTables[fieldName];
-
-    if (fieldRef == null) {
-      fieldsValues.putIfAbsent(fieldName, () => value);
-      var valueSQL = _conditionSQLGenerator.parameterPlaceholder(fieldName);
-      return valueSQL;
-    } else {
-      var refEntity = value;
-      var refEntityRepository = getEntityRepository(obj: refEntity);
-
-      if (refEntityRepository != null) {
-        var refId = refEntityRepository.entityHandler
-            .getField(value, fieldRef.targetField);
-
-        if (refId != null) {
-          fieldsValues.putIfAbsent(fieldName, () => refId);
-          var valueSQL = _conditionSQLGenerator.parameterPlaceholder(fieldName);
-          return valueSQL;
-        } else {
-          var retRefId = refEntityRepository.store(refEntity,
-              transaction: context.transaction);
-          return retRefId.resolveMapped((refId) {
-            fieldsValues.putIfAbsent(fieldName, () => refId);
-            var valueSQL =
-                _conditionSQLGenerator.parameterPlaceholder(fieldName);
-            return valueSQL;
-          });
-        }
-      } else {
-        if (value is Entity) {
-          var refId = value.getID();
-
-          fieldsValues.putIfAbsent(fieldName, () => refId);
-          var valueSQL = _conditionSQLGenerator.parameterPlaceholder(fieldName);
-          return valueSQL;
-        } else {
-          var reflection =
-              ReflectionFactory().getRegisterClassReflection(value.runtimeType);
-          if (reflection != null) {
-            var fieldId = tableScheme.idFieldName ?? 'id';
-
-            var refId = reflection.getField(fieldId, value);
-
-            fieldsValues.putIfAbsent(fieldName, () => refId);
-            var valueSQL =
-                _conditionSQLGenerator.parameterPlaceholder(fieldName);
-            return valueSQL;
-          }
-        }
-
-        return 'null';
-      }
-    }
-  }
-
-  FutureOr<int> countSQL(TransactionOperation op, String table, SQL sql) {
-    return executeTransactionOperation(op, (connection) {
-      _log.log(logging.Level.INFO, 'countSQL> $sql');
-      return doCountSQL(table, sql, connection);
-    });
-  }
-
-  FutureOr<int> doCountSQL(
-    String table,
-    SQL sql,
-    C connection,
-  );
-
-  FutureOr<dynamic> insertSQL(TransactionOperation op, String table, SQL sql,
-      Map<String, Object?> fields,
-      {T Function<T>(dynamic o)? mapper}) {
-    return executeTransactionOperation(op, (connection) {
-      _log.log(logging.Level.INFO, 'insertSQL> $sql');
-      var retInsert = doInsertSQL(table, sql, connection);
-
-      if (mapper != null) {
-        return retInsert.resolveMapped((e) => mapper(e));
-      } else {
-        return retInsert;
-      }
-    });
-  }
-
   FutureOr<dynamic> updateSQL(TransactionOperation op, String table, SQL sql,
       Object id, Map<String, Object?> fields,
       {T Function<T>(dynamic o)? mapper}) {
     return executeTransactionOperation(op, (connection) {
       _log.log(logging.Level.INFO, 'updateSQL> $sql');
-      var retInsert = doInsertSQL(table, sql, connection);
+      var retInsert = doUpdateSQL(table, sql, connection);
 
       if (mapper != null) {
         return retInsert.resolveMapped((e) => mapper(e));
@@ -683,7 +552,109 @@ abstract class SQLAdapter<C> extends SchemeProvider
           TransactionOperation op, FutureOr<R> Function(C connection) f) =>
       executeWithPool(f);
 
-  FutureOr<dynamic> doInsertSQL(String table, SQL sql, C connection);
+  FutureOr<SQL> generateSelectSQL(
+      Transaction transaction, String table, EntityMatcher matcher,
+      {Object? parameters,
+      List? positionalParameters,
+      Map<String, Object?>? namedParameters,
+      int? limit}) {
+    return _generateSQLFrom(transaction, table, matcher,
+        parameters: parameters,
+        positionalParameters: positionalParameters,
+        namedParameters: namedParameters,
+        sqlBuilder: (String from, EncodingContext context) {
+      var tableAlias = context.resolveEntityAlias(table);
+      var limitStr = limit != null && limit > 0 ? ' LIMIT $limit' : '';
+      var sql = 'SELECT "$tableAlias".* $from$limitStr';
+      return sql;
+    });
+  }
+
+  FutureOr<SQL> _generateSQLFrom(
+      Transaction transaction, String table, EntityMatcher matcher,
+      {Object? parameters,
+      List? positionalParameters,
+      Map<String, Object?>? namedParameters,
+      required String Function(String from, EncodingContext context)
+          sqlBuilder}) {
+    if (matcher is Condition) {
+      var retEncodedSQL = _conditionSQLGenerator.encode(matcher, table,
+          parameters: parameters,
+          positionalParameters: positionalParameters,
+          namedParameters: namedParameters);
+
+      return retEncodedSQL.resolveMapped((encodedSQL) {
+        var conditionSQL = encodedSQL.outputString;
+
+        var tableAlias =
+            _conditionSQLGenerator.resolveEntityAlias(encodedSQL, table);
+
+        if (encodedSQL.fieldsReferencedTables.isEmpty) {
+          String from;
+          if (conditionSQL.isNotEmpty) {
+            from = 'FROM "$table" as "$tableAlias" WHERE $conditionSQL';
+          } else {
+            from = 'FROM $table as "$tableAlias"';
+          }
+
+          var sqlQuery = sqlBuilder(from, encodedSQL);
+
+          return SQL(sqlQuery, encodedSQL.parametersPlaceholders,
+              condition: matcher,
+              entityName: encodedSQL.entityName,
+              mainTable: table);
+        } else {
+          var referencedTablesFields = encodedSQL.referencedTablesFields;
+
+          var innerJoin = StringBuffer();
+
+          for (var e in referencedTablesFields.entries) {
+            var refTable = e.key;
+
+            var refTableAlias = encodedSQL.resolveEntityAlias(refTable);
+
+            innerJoin.write('INNER JOIN "$refTable" as "$refTableAlias" ON ');
+
+            for (var fieldRef in e.value) {
+              var sourceTableAlias = _conditionSQLGenerator.resolveEntityAlias(
+                  encodedSQL, fieldRef.sourceTable);
+              var targetTableAlias = _conditionSQLGenerator.resolveEntityAlias(
+                  encodedSQL, fieldRef.targetTable);
+
+              innerJoin.write('"');
+              innerJoin.write(sourceTableAlias);
+              innerJoin.write('"');
+              innerJoin.write('.');
+              innerJoin.write('"');
+              innerJoin.write(fieldRef.sourceField);
+              innerJoin.write('"');
+
+              innerJoin.write(' = ');
+
+              innerJoin.write('"');
+              innerJoin.write(targetTableAlias);
+              innerJoin.write('"');
+              innerJoin.write('.');
+              innerJoin.write('"');
+              innerJoin.write(fieldRef.targetField);
+              innerJoin.write('"');
+            }
+          }
+
+          var from =
+              'FROM "$table" as "$tableAlias" $innerJoin WHERE $conditionSQL';
+          var sqlQuery = sqlBuilder(from, encodedSQL);
+
+          return SQL(sqlQuery, encodedSQL.parametersPlaceholders,
+              condition: matcher,
+              entityName: encodedSQL.entityName,
+              mainTable: table);
+        }
+      });
+    } else {
+      throw StateError('$matcher');
+    }
+  }
 
   FutureOr<Iterable<Map<String, dynamic>>> selectSQL(
       TransactionOperation op, String table, SQL sql,
@@ -706,6 +677,35 @@ abstract class SQLAdapter<C> extends SchemeProvider
     SQL sql,
     C connection,
   );
+
+  FutureOr<SQL> generateDeleteSQL(
+      Transaction transaction, String table, EntityMatcher matcher,
+      {Object? parameters,
+      List? positionalParameters,
+      Map<String, Object?>? namedParameters}) {
+    return _generateSQLFrom(transaction, table, matcher,
+        parameters: parameters,
+        positionalParameters: positionalParameters,
+        namedParameters: namedParameters,
+        sqlBuilder: (String from, EncodingContext context) {
+      var tableAlias = context.resolveEntityAlias(table);
+
+      var sql = StringBuffer();
+      sql.write('DELETE ');
+
+      if (sqlAcceptsInsertOutput) {
+        sql.write(' OUTPUT DELETED.* ');
+      }
+
+      sql.write(from);
+
+      if (sqlAcceptsInsertReturning) {
+        sql.write(' RETURNING "$tableAlias".*');
+      }
+
+      return sql.toString();
+    });
+  }
 
   FutureOr<Iterable<Map<String, dynamic>>> deleteSQL(
       TransactionOperation op, String table, SQL sql,
