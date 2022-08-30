b12561ddbfc39c5ffb16373a74379eca191ad04c
gmpassos <gracilianomp@gmail.com>
2022-08-30 20:18:47 -0300

    v1.3.7 - Added `EntityReference`:   An entity field wrapper that allows lazy load of sub-entities. - Added `EntityResolutionRules` to allow lazy or eager selects. - reflection_factory: ^1.2.8

--- test/bones_api_test_entities.reflection.g.dart
+++ test/bones_api_test_entities.reflection.g.dart
@@ -1,6 +1,6 @@
 //
 // GENERATED CODE - DO NOT MODIFY BY HAND!
-// BUILDER: reflection_factory/1.2.7
+// BUILDER: reflection_factory/1.2.8
 // BUILD COMMAND: dart run build_runner build
 //
 
@@ -62,7 +62,7 @@ class Address$reflection extends ClassReflection<Address> {
   Version get languageVersion => Version.parse('2.17.0');
 
   @override
-  Version get reflectionFactoryVersion => Version.parse('1.2.7');
+  Version get reflectionFactoryVersion => Version.parse('1.2.8');
 
   @override
   Address$reflection withObject([Address? obj]) => Address$reflection(obj);
@@ -551,7 +551,7 @@ class Role$reflection extends ClassReflection<Role> {
   Version get languageVersion => Version.parse('2.17.0');
 
   @override
-  Version get reflectionFactoryVersion => Version.parse('1.2.7');
+  Version get reflectionFactoryVersion => Version.parse('1.2.8');
 
   @override
   Role$reflection withObject([Role? obj]) => Role$reflection(obj);
@@ -972,7 +972,7 @@ class RoleType$reflection extends EnumReflection<RoleType> {
   Version get languageVersion => Version.parse('2.17.0');
 
   @override
-  Version get reflectionFactoryVersion => Version.parse('1.2.7');
+  Version get reflectionFactoryVersion => Version.parse('1.2.8');
 
   @override
   RoleType$reflection withObject([RoleType? obj]) => RoleType$reflection(obj);
@@ -1036,7 +1036,7 @@ class Store$reflection extends ClassReflection<Store> {
   Version get languageVersion => Version.parse('2.17.0');
 
   @override
-  Version get reflectionFactoryVersion => Version.parse('1.2.7');
+  Version get reflectionFactoryVersion => Version.parse('1.2.8');
 
   @override
   Store$reflection withObject([Store? obj]) => Store$reflection(obj);
@@ -1449,7 +1449,7 @@ class User$reflection extends ClassReflection<User> {
   Version get languageVersion => Version.parse('2.17.0');
 
   @override
-  Version get reflectionFactoryVersion => Version.parse('1.2.7');
+  Version get reflectionFactoryVersion => Version.parse('1.2.8');
 
   @override
   User$reflection withObject([User? obj]) => User$reflection(obj);
@@ -1992,7 +1992,7 @@ class UserInfo$reflection extends ClassReflection<UserInfo> {
   Version get languageVersion => Version.parse('2.17.0');
 
   @override
-  Version get reflectionFactoryVersion => Version.parse('1.2.7');
+  Version get reflectionFactoryVersion => Version.parse('1.2.8');
 
   @override
   UserInfo$reflection withObject([UserInfo? obj]) => UserInfo$reflection(obj);
