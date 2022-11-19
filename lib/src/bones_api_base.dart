ffde111c7e38985e3f8b63e16829f16a2ba11975
gmpassos <gracilianomp@gmail.com>
2022-11-19 03:15:26 -0300

    v1.3.25 - `APISecurity`:   - Added `disposeAuthenticationData`. - swiss_knife: ^3.1.2 - pubspec: ^2.3.0 - coverage: ^1.6.1

--- lib/src/bones_api_base.dart
+++ lib/src/bones_api_base.dart
@@ -39,7 +39,7 @@ typedef APILogger = void Function(APIRoot apiRoot, String type, String? message,
 /// Bones API Library class.
 class BonesAPI {
   // ignore: constant_identifier_names
-  static const String VERSION = '1.3.24';
+  static const String VERSION = '1.3.25';
 
   static bool _boot = false;
 
