37d9e6aa2ebd2b23bc1ae0fd455ba089148069b3
gmpassos <gracilianomp@gmail.com>
2023-12-08 06:47:15 -0300

    v1.5.15

--- lib/src/bones_api_base.dart
+++ lib/src/bones_api_base.dart
@@ -42,7 +42,7 @@ typedef APILogger = void Function(APIRoot apiRoot, String type, String? message,
 /// Bones API Library class.
 class BonesAPI {
   // ignore: constant_identifier_names
-  static const String VERSION = '1.5.14';
+  static const String VERSION = '1.5.15';
 
   static bool _boot = false;
 
